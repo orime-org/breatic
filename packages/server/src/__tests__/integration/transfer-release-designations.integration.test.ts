72a674fe57e593b35c412d421265f0137ef1924e
ubiquition <45891087@qq.com>
2026-08-25 11:27:12 +0800

    chore: carry the renamed license identifier into the new test file

--- packages/server/src/__tests__/integration/transfer-release-designations.integration.test.ts
+++ packages/server/src/__tests__/integration/transfer-release-designations.integration.test.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 Orime, Inc.
-// SPDX-License-Identifier: LicenseRef-BOSL-1.0
+// SPDX-License-Identifier: LicenseRef-BSAL-1.0
 
 /**
  * 失去 admin 身份即解除积分包指定（任务 #15），钉在真实 Postgres 上。
