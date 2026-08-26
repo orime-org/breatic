2142b2cbba082c722a6228b0fde2b55ecd346acf
ubiquition <45891087@qq.com>
2026-08-26 08:21:48 +0800

    chore: carry the renamed licence identifier into this slice's files

--- packages/web/src/spaces/document/DocumentLinkPopover.tsx
+++ packages/web/src/spaces/document/DocumentLinkPopover.tsx
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 Orime, Inc.
-// SPDX-License-Identifier: LicenseRef-BOSL-1.0
+// SPDX-License-Identifier: LicenseRef-BSAL-1.0
 
 /**
  * The link control: a button on the selection bubble bar, and the panel it
--- packages/web/src/spaces/document/__tests__/document-link-locales.test.ts
+++ packages/web/src/spaces/document/__tests__/document-link-locales.test.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 Orime, Inc.
-// SPDX-License-Identifier: LicenseRef-BOSL-1.0
+// SPDX-License-Identifier: LicenseRef-BSAL-1.0
 
 /**
  * The six strings the link control puts on screen, in all five catalogs.
